5b188203effa9380cced2b151c86856f68ee24ca
urmilarawat005 <u4urmilarawat@gmail.com>
2025-01-14 10:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
+Person prototype.greet = function(){
+		console.log(`hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	Person.call(this, name, age);
+	this.jobtitle = jobTitle;
+}
+
+Employee.prototype.jobGreet = function(){
+		console.log(`hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+};
+
+
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
+
+
+
+
+
+
+
+
+
+
+
+
